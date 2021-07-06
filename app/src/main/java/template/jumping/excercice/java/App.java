f0019526f6857d09132cb267840ed73a4981e897
Jona Ocles <oclesjo@gmail.com>
2021-07-06 16:49:26 -0500

    Added test solution

--- app/src/main/java/template/jumping/excercice/java/App.java
+++ app/src/main/java/template/jumping/excercice/java/App.java
@@ -1,16 +1,60 @@
 package template.jumping.excercice.java;
 
+import java.util.Arrays;
+
 public class App {
 
     public static void main(String[] args) {
-        String arrayObstacles = args[0];
-        
-        Integer minimumJump = 0;
 
-        //TODO: Implement solution
-        
+        int[] arrayObstacles = getOrderedIntegerArray(args[0]);
+
+        Integer minimumJump = getMinimumJump(arrayObstacles);
+
         System.out.println("The minimum jump to avoid all obstacles is: " + minimumJump);
     }
+
+    private static Integer getMinimumJump(int[] arrayObstacles) {
+        int maxNumber= 0;
+
+        for (int i = 0; i < arrayObstacles.length; i++) {
+            maxNumber = Math.max(maxNumber, arrayObstacles[i]);
+        }
+
+        for (int i = 1; i <= maxNumber ; i++) {
+            int j = i;
+            for(;j<=maxNumber;j=j+i){
+                if(isContainedInArray(j, arrayObstacles)){
+                    break;
+                }
+            }
+            if(j>maxNumber){
+                return i;
+            }
+        }
+
+        return maxNumber+1;
+    }
+
+    private static boolean isContainedInArray(int j, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if(j==array[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private static int[] getOrderedIntegerArray(String data) {
+        String [] splitData = data.split(",");
+        int [] integers = new int[splitData.length];
+        for (int i = 0; i < splitData.length; i++) {
+            integers[i]=Integer.valueOf(splitData[i]);
+        }
+        Arrays.sort(integers);
+        return integers;
+    }
+
 
     public String getGreeting() {
         return "Hello World!";
